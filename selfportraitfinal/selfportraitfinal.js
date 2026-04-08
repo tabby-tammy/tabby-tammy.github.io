99c8150828c843f0eb53acf0fefb56f274d8124d
tabby-tammy <itschopagain@gmail.com>
2026-04-08 14:41:18 -0700

    think emoji

---
+++ selfportraitfinal/selfportraitfinal.js
@@ -0,0 +1,658 @@
+function setup() {
+createCanvas(600, 600);
+background(216, 218, 217)
+
+}
+
+
+function draw() {
+
+  //back hair
+      noStroke();
+  fill(52, 44, 41);
+  beginShape();
+  
+  vertex(116, 320);
+  vertex(135, 353);
+  vertex(138, 374);
+  vertex(143, 402);
+  vertex(148, 420);
+  vertex(142, 440);
+  vertex(143, 455);
+  vertex(147, 440);
+  vertex(155, 453);
+  vertex(157, 464);
+  vertex(161, 458), 
+  vertex(171, 469);
+  vertex(172, 455);
+  vertex(182, 446);
+  vertex(170, 388);
+  vertex(157, 343);
+  vertex(143, 250);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(52, 44, 41);
+  beginShape();
+  
+  vertex(419, 415);
+  vertex(426, 453);
+  vertex(422, 467);
+  vertex(415, 474);
+  vertex(395, 482);
+  vertex(398, 451);
+  
+  endShape(CLOSE);
+
+  noStroke();
+  fill(52, 44, 41);
+  beginShape();
+
+  vertex(446, 366);
+  vertex(451, 381);
+  vertex(448, 396);
+  vertex(440, 406);
+  vertex(425, 413);
+  vertex(434, 389);
+  
+  endShape(CLOSE);
+  
+// back hoodie
+   noStroke();
+  fill(20, 21, 23);
+  beginShape();
+  
+  vertex(196, 485);
+  vertex(168, 476);
+  vertex(153, 476);
+  vertex(148, 477);
+  vertex(143, 468);
+  vertex(127, 463);
+  vertex(101, 464);
+  vertex(80, 477);
+  vertex(78, 484);
+  vertex(142, 524);
+  vertex(192, 539);
+  
+  endShape(CLOSE);
+  
+  // skin
+    noStroke();
+  fill(195, 136, 106);
+  beginShape();
+  
+  vertex(175, 419);
+  vertex(180, 474);
+  vertex(179, 497);
+  vertex(167, 527);
+  vertex(287, 600);
+  vertex(404, 562);
+  vertex(400, 505);
+  vertex(406, 483);
+  vertex(410, 447);
+  vertex(369, 420);
+  vertex(418,434);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(225, 172, 147);
+  beginShape();
+  
+  vertex(315, 53);
+  vertex(209, 134);
+  vertex(137, 260);
+  vertex(147, 312);
+  vertex(152, 369);
+  vertex(176, 434);
+  vertex(222, 472);
+  vertex(284, 493);
+  vertex(349, 486);
+  vertex(398, 469);
+  vertex(420, 431); 
+  vertex(430, 411);
+  vertex(445, 377);
+  vertex(452, 353);
+  vertex(454, 316);
+  vertex(467, 292);
+  vertex(451, 161);
+ vertex(420, 79);
+  
+  endShape(CLOSE);
+  
+    noStroke();
+  fill(195, 136, 106);
+  beginShape();
+  
+  vertex(227, 433);
+  vertex(250, 452);
+  vertex(291, 461);
+  vertex(338, 452);
+  vertex(375, 433);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(225, 172, 147);
+  beginShape();
+  
+  vertex(440, 377);
+  vertex(455, 369);
+  vertex(461, 349);
+  vertex(461, 327);
+  vertex(440, 344);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(225, 172, 147);
+  beginShape();
+  
+  vertex(178, 440);
+  vertex(200, 482);
+  vertex(278, 525);
+  vertex(328, 516);
+  vertex(400, 476);
+  vertex(405, 539);
+  vertex(355, 600);
+  vertex(191, 600);
+  vertex(161, 532);
+  vertex(174, 506);
+  vertex(179, 482);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(195, 136, 106);
+  beginShape();
+  
+  vertex(255, 386);
+  vertex(281, 392);
+  vertex(302, 393);
+  vertex(326, 395);
+  vertex(348, 389);
+  vertex(322, 408);
+  vertex(277, 409);
+  
+  endShape(CLOSE);
+  
+  // hair  
+  noStroke();
+  fill(14, 6, 6);
+  beginShape();
+  
+  vertex(364, 20);
+  vertex(306, 63);
+  vertex(400, 81);
+  vertex(434, 142);
+  vertex(442, 210); 
+  vertex(462, 274);
+  vertex(450, 348);
+  vertex(462, 380); 
+  vertex(485, 348); 
+  vertex(479, 320); 
+  vertex(499, 292); 
+  vertex(511, 243); 
+  vertex(502, 174); 
+  vertex(478, 120);
+  vertex(416, 53);
+  
+  endShape(CLOSE);
+  
+    noStroke();
+  fill(52, 44, 41);
+  beginShape();
+  
+  vertex(364, 20);
+  vertex(184, 193);
+  vertex(123, 376);
+  vertex(104, 191);
+  vertex(139, 107);
+  vertex(200, 49); 
+  
+  endShape(CLOSE);
+  
+  //shirt 
+  
+    noStroke();
+  fill(117, 161, 177);
+  beginShape();
+  
+  vertex(271, 592);
+  vertex(313, 592);
+  vertex(305, 600);
+  vertex(274, 600);
+  
+  endShape(CLOSE);
+  
+  // hoodie
+  
+  noStroke();
+  fill(52, 44, 41);
+  beginShape();
+  
+  vertex(494, 557);
+  vertex(535, 558);
+  vertex(552, 564);
+  vertex(571, 569);
+  vertex(587, 581);
+  vertex(600, 582);
+  vertex(600, 600);
+  vertex(465, 600);
+  
+  endShape(CLOSE);
+
+  noStroke();
+  fill(20, 21, 23);
+  beginShape();
+  
+  vertex(504, 558);
+  vertex(487, 600);
+  vertex(512, 600);
+  vertex(524, 560);
+  
+  endShape(CLOSE);
+
+  noStroke();
+  fill(20, 21, 23);
+  beginShape();
+  
+  vertex(71, 523);
+  vertex(96, 538);
+  vertex(123, 566);
+  vertex(144, 580);
+  vertex(180, 600);
+  vertex(141, 600);
+  vertex(123, 582);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(52, 44, 41);
+  beginShape();
+  
+  vertex(300, 600);
+  vertex(335, 562);
+  vertex(385, 512);
+  vertex(399, 478);
+  vertex(431, 486);
+  vertex(443, 505);
+  vertex(442, 508);
+  vertex(451, 520);
+  vertex(464, 516);
+  vertex(476, 513);
+  vertex(490, 516);
+  vertex(498, 525);
+  vertex(498, 538);
+  vertex(491, 551);
+  vertex(507, 559);
+  vertex(510, 574);
+  vertex(505, 588);
+  vertex(499, 600);
+  
+  endShape(CLOSE);
+  
+    noStroke();
+  fill(52, 44, 41);
+  beginShape();
+  
+  vertex(286, 600);
+  vertex(259, 578);
+  vertex(227, 556);
+  vertex(184, 529);
+  vertex(161, 509);
+  vertex(138, 485);
+  vertex(115, 471);
+  vertex(86, 472);
+  vertex(61, 482);
+  vertex(42, 498);
+  vertex(30, 519);
+  vertex(62, 527);
+  vertex(91, 547);
+  vertex(121, 573);
+  vertex(153, 594);
+  vertex(178, 600);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(20, 21, 23);
+  beginShape();
+  
+  vertex(437, 519);
+  vertex(412, 553);
+  vertex(341, 588);
+  vertex(325, 600);
+  vertex(373, 600);
+  vertex(402, 594);
+  vertex(422, 579);
+  vertex(435, 560);
+  vertex(446, 547);
+  vertex(470, 515);
+  
+  endShape(CLOSE);
+
+  noStroke();
+  fill(20, 21, 23);
+  beginShape();
+  
+  vertex(38, 519);
+  vertex(53, 507);
+  vertex(78, 502);
+  vertex(106, 519);
+  vertex(134, 536);
+  vertex(150, 547);
+  vertex(163, 562);
+  vertex(147, 555);
+  vertex(128, 543);
+  vertex(109, 533);
+  vertex(90, 519);
+  vertex(72, 510);
+  vertex(59, 513);
+  vertex(55, 521);
+  
+  endShape(CLOSE);
+
+  noStroke();
+  fill(20, 21, 23);
+  beginShape();
+  
+  vertex(89, 541);
+  vertex(93, 600);
+  vertex(164, 600);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(52, 44, 41);
+  beginShape();
+  
+  vertex(54, 520);
+  vertex(81, 534)
+  vertex(98, 553);
+  vertex(104, 578);
+  vertex(103, 600);
+  vertex(0, 600);
+  vertex(0, 528);
+  vertex(35, 520);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(20, 21, 23);
+  beginShape();
+  
+  vertex(32, 519);
+  vertex(42, 548);
+  vertex(48, 576);
+  vertex(46, 600);
+  vertex(62, 600);
+  vertex(59, 570);
+  vertex(59, 539);
+  vertex(49, 531);
+  vertex(46, 520);
+  
+  endShape(CLOSE);
+  
+  // lips
+  
+  noStroke();
+  fill(113, 33, 26);
+  beginShape();
+  
+  vertex(246, 354);
+  vertex(246, 379);
+  vertex(269, 366);
+  vertex(327, 371);
+  vertex(356, 369);
+  vertex(363, 374);
+  vertex(360, 349);
+  vertex(347, 363);
+  vertex(257, 360);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(200, 114, 109);
+  beginShape();
+  
+  vertex(251, 370);
+  vertex(275, 364);
+  vertex(295, 367);
+  vertex(315, 369);
+  vertex(349, 369);
+  vertex(343, 379);
+  vertex(313, 385);
+  vertex(287, 383);
+  vertex(273, 379);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(153, 84, 69);
+  beginShape();
+  
+  vertex(267, 353);
+  vertex(264, 360);
+  vertex(269, 349);
+  vertex(306, 352);
+  vertex(317, 350);
+  vertex(326, 350);
+  vertex(337, 355);
+  vertex(346, 360);
+  vertex(356, 367);
+  vertex(304, 366);
+  vertex(257, 362);
+  
+  endShape(CLOSE);
+  
+  //nose
+   noStroke();
+  fill(234, 180, 151);
+  beginShape();
+  
+  vertex(259, 292);
+  vertex(275, 310);
+  vertex(302, 315);
+  vertex(323, 315);
+  vertex(343, 310);
+  vertex(349, 296);
+  vertex(337, 277);
+  vertex(329, 252);
+  vertex(327, 219);
+  vertex(323, 202);
+  vertex(298, 199);
+  vertex(291, 219);
+  vertex(285, 243);
+  vertex(278, 266);
+  vertex(269, 280);
+  
+  endShape(CLOSE);
+  
+    noStroke();
+  fill(195, 136, 106);
+  beginShape();
+  
+  vertex(260, 302);
+  vertex(292, 291);
+  vertex(315, 290);
+  vertex(344, 308);
+  vertex(324, 316);
+  vertex(311, 319);
+  vertex(292, 318);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(87, 36, 18);
+  beginShape();
+  
+  vertex(292, 317);
+  vertex(276, 314);
+  vertex(271, 302);
+  vertex(285, 306);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(87, 36, 18);
+  beginShape();
+  
+  vertex(312, 316);
+  vertex(322, 305);
+  vertex(336, 307);
+  vertex(329, 315);
+  
+  endShape(CLOSE);
+  // glasses
+  
+  strokeWeight(8);
+  stroke(246, 221, 181);
+  noFill();
+  beginShape();
+  
+  vertex(161, 189);
+  vertex(160, 220);
+  vertex(166, 250);
+  vertex(177, 271);
+  vertex(202, 279);
+  vertex(242, 280);
+  vertex(270, 269);
+  vertex(286, 246);
+  vertex(290, 209);
+  vertex(272, 190);
+  vertex(242, 181);
+  vertex(202, 179);
+  
+  endShape(CLOSE);
+  
+  strokeWeight(8);
+  stroke(246, 221, 181);
+  noFill();
+  beginShape();
+  
+  vertex(330, 207);
+  vertex(351, 190);
+  vertex(381, 183);
+  vertex(413, 184);
+  vertex(443, 190);
+  vertex(458, 200);
+  vertex(457, 228);
+  vertex(449, 253);
+  vertex(439, 275);
+  vertex(426, 281);
+  vertex(394, 289);
+  vertex(362, 282);
+  vertex(341, 272);
+  vertex(331, 251);
+  vertex(327, 244);
+  
+  endShape(CLOSE);
+  
+  strokeWeight(8);
+  stroke(246, 221, 181);
+  noFill();
+  beginShape();
+  
+  vertex(287, 210);
+  vertex(303, 206);
+  vertex(315, 207);
+  vertex(328, 210);
+  
+  endShape(OPEN);
+  
+  // eyebrows
+  
+  noStroke();
+  fill(52, 44, 41);
+  beginShape();
+  
+  vertex(270, 174);
+  vertex(252, 160);
+  vertex(222, 154);
+  vertex(183, 156);
+  vertex(183, 177);
+  vertex(218, 172);
+  vertex(242, 173);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(52, 44, 41);
+  beginShape();
+  
+  vertex(351, 176);
+  vertex(371, 160);
+  vertex(400, 157);
+  vertex(423, 164);
+  vertex(435, 182);
+  vertex(434, 197);
+  vertex(415, 180);
+  vertex(396, 177);
+  vertex(379, 179);
+  vertex(365, 181);
+  
+  endShape(CLOSE);
+  
+  // eyes
+  
+  noStroke();
+  fill(163, 132, 119);
+  beginShape();
+  
+  vertex(258, 216);
+  vertex(247, 209);
+  vertex(232, 205);
+  vertex(218, 209);
+  vertex(207, 213);
+  vertex(195, 225);
+  vertex(233, 220);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(163, 132, 119);
+  beginShape();
+  
+  vertex(355, 219);
+  vertex(372, 208);
+  vertex(396, 210);
+  vertex(406, 218);
+  vertex(415, 227);
+  vertex(382, 220);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(17, 9, 12);
+  beginShape();
+  
+  vertex(351, 223);
+  vertex(371, 218);
+  vertex(389, 220);
+  vertex(405, 226);
+  vertex(416, 232);
+  vertex(395, 231);
+  vertex(403, 231);
+  vertex(375, 230);
+  
+  endShape(CLOSE);
+  
+  noStroke();
+  fill(17, 9, 12);
+  beginShape();
+  
+  vertex(260, 224);
+  vertex(249, 217);
+  vertex(236, 214);
+  vertex(217, 217);
+  vertex(205, 221);
+  vertex(200, 229);
+  vertex(215, 227);
+  vertex(235, 226);
+  vertex(251, 227);
+  
+  endShape(CLOSE);
+  
+}
